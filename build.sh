52f0fcd43446ad8b4ad1865a125dfe21e50ea65a
matzeton <matzeton@googlemail.com>
2016-05-24 11:12:01 +0000

    - renamed raspicam

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 DIR=$(dirname $0)
+echo "* ${DIR}"
 
-$DIR/env.sh
-$DIR/deps.sh
+#. ${DIR}/env.sh
+sudo $DIR/deps.sh
+
+OLDPWD=$(pwd)
+
+cd ${DIR}/cvblob
+dpkg-buildpackage -b -us -uc
+cd ${OLDPWD}
+
+cd ${DIR}/raspicam
+dpkg-buildpackage -b -us -uc
+cd ${OLDPWD}
