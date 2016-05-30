cd2529f3eef42eb37c043d0ca76fab0bdabcabca
matzeton <matzeton@googlemail.com>
2016-05-30 15:43:49 +0000

    support for already processed elements

--- src/RC_CircularBuffer.hpp
+++ src/RC_CircularBuffer.hpp
@@ -3,9 +3,12 @@
  * Author: Toni Uhlig <matzeton@googlemail.com>
  */
 
+/* ACHTUNG: Diese Klasse ist _NICHT_ `Threadsafe`, d.h. in Multithreaded Umgebungen muss der Entwickler entsprechende Synchronisationsmaßnahmen ergreifen (siehe RC_BlobDetectorFactor) */
+
 #ifndef RC_CIRCULARBUFFER_H
 #define RC_CIRCULARBUFFER_H 1
 
+#include <iostream>
 #include <vector>
 
 
@@ -14,24 +17,32 @@ template <class T>
 class CircularBuffer {
 
   private:
-    T* cBuffer; // Array der Länge "maxElements"
+    T * cBuffer; // Array der Länge "maxElements"
     unsigned int nextIndex; // Feldindex, welcher beim nächsten addElement(...) überschrieben wird
     unsigned int maxElements; // maximale Größe des Puffers
+    bool * alreadyProcessedElements; // Bool- Feld, welches bereits durch `getElement` zurückgegebe Elemente mit `true` markiert
 
   public:
     CircularBuffer(unsigned int maxElements) {
       nextIndex = 0;
       this->maxElements = maxElements;
       cBuffer = new T[maxElements]; // erzeuge Feld der Größe n (maxElements)
+      alreadyProcessedElements = new bool[maxElements];
+      for (auto i = maxElements; i > 0; --i) {
+        alreadyProcessedElements[i % maxElements] = false;
+      }
     }
 
     ~CircularBuffer(void) {
       delete[] cBuffer;
+      delete[] alreadyProcessedElements;
     }
 
     void addElement(T elem) {
       /* überschreibe das Element an Position "nextIndex" */
       cBuffer[nextIndex] = elem;
+      /* den entsprechende Index in `alreadyProcessedElements` auf `false` setzen */
+      alreadyProcessedElements[nextIndex] = false;
       /* nextIndex inkrementieren und dabei auf die Grenze "maxElements" beachten */
       /* % = Modulo */
       auto tmp = nextIndex;
@@ -43,8 +54,16 @@ class CircularBuffer {
       return nextIndex;
     }
 
-    T getCurrentElement(void) {
-      return cBuffer[nextIndex];
+    bool getElement(T arg) {
+      for (auto i = nextIndex; i < nextIndex+maxElements; ++i) {
+        auto index = i % maxElements;
+        if (alreadyProcessedElements[index] == false) {
+          alreadyProcessedElements[index] = true;
+          arg = cBuffer[index];
+          return true;
+        }
+      }
+      return false; // alle Elemente im Puffer wurden bereits mit `getElement()` zurückgegeben
     }
 
 };
