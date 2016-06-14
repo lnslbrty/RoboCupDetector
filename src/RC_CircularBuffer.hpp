b1a612b13f05b451bbda1d40b2e28a6350ffd80c
matzeton <matzeton@googlemail.com>
2016-06-14 02:05:11 +0000

    CircularBuffer is now multithread compatible

--- src/RC_CircularBuffer.hpp
+++ src/RC_CircularBuffer.hpp
@@ -3,13 +3,13 @@
  * Author: Toni Uhlig <matzeton@googlemail.com>
  */
 
-/* ACHTUNG: Diese Klasse ist _NICHT_ `Threadsafe`, d.h. in Multithreaded Umgebungen muss der Entwickler entsprechende Synchronisationsmaßnahmen ergreifen (siehe RC_BlobDetectorFactory) */
 
 #ifndef RC_CIRCULARBUFFER_H
 #define RC_CIRCULARBUFFER_H 1
 
 #include <iostream>
 #include <vector>
+#include <mutex>
 
 
 namespace rc {
@@ -17,6 +17,7 @@ template <class T>
 class CircularBuffer {
 
   private:
+    std::mutex mtx;
     T * cBuffer; // Array der Länge "maxElements"
     unsigned int nextIndex; // Feldindex, welcher beim nächsten addElement(...) überschrieben wird
     unsigned int maxElements; // maximale Größe des Puffers
@@ -41,6 +42,7 @@ class CircularBuffer {
     }
 
     void addElement(T& elem) {
+      std::unique_lock<std::mutex> lck(mtx);
       /* überschreibe das Element an Position "nextIndex" */
       cBuffer[nextIndex] = elem;
       /* den entsprechende Index in `alreadyProcessedElements` auf `false` setzen */
@@ -55,10 +57,17 @@ class CircularBuffer {
     }
 
     unsigned int getMaxElementCount(void) { return maxElements; }
-    unsigned int getNextIndex(void) { return nextIndex; }
-    unsigned int getElementCount(void) { return availableElements; }
+    unsigned int getNextIndex(void) { 
+      std::unique_lock<std::mutex> lck(mtx);
+      return nextIndex;
+    }
+    unsigned int getElementCount(void) {
+      std::unique_lock<std::mutex> lck(mtx);
+      return availableElements;
+    }
 
     bool getElement(T& arg) {
+      std::unique_lock<std::mutex> lck(mtx);
       if (availableElements == 0) return false; // noch keine Elemente im Puffer
       for (auto i = nextIndex; i < nextIndex+maxElements; ++i) {
         auto index = i % maxElements;
