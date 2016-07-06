e583a8273e31d28dd597074e31d0558924f0291d
matzeton <matzeton@googlemail.com>
2016-07-06 18:31:23 +0000

    - providing stable dependencies instead of unstable official ones - fixed BUG with raspicam (color bug)

--- deps.sh
+++ deps.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $(id -u) -ne 0 ]; then
+	echo "$0: Dieses Skript kann nur als root ausgeführt werden."
+	echo "Befehl: sudo $0"
+	exit 1
+fi
+
+dpkg -i $(dirname $0)/dependencies/*.deb
 apt-get install gcc autoconf cmake make cmake-curses-gui
 apt-get install dh-make build-essential
 apt-get install devscripts fakeroot debootstrap pbuilder
