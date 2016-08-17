3f1721b2bb95e7cffec3e99332921181cff734ac
matzeton <matzeton@googlemail.com>
2016-08-17 16:52:30 +0000

    threadcount command line parameter + documentation

--- src/RC_Main.cpp
+++ src/RC_Main.cpp
@@ -16,6 +16,7 @@
 #include "RC_Daemon.hpp"
 #include "RC_BlobDetectorFactory.hpp"
 
+/** Vorschaufenster Abhängigkeit */
 #if defined(USE_XWINDOW_FLTRD) && !defined(USE_XWINDOW)
 #error "USE_XWINDOW_FLTRD needs USE_XWINDOW"
 #endif
@@ -48,15 +49,21 @@ struct cmd_opts {
   uint8_t sat;                  /** Farbsättigung */
   uint8_t gain;                 /** Bildverstärkung */
   int8_t exp;                   /** Belichtungsdauer */
+  uint8_t thrds;                /** RoboCup Bilderkennungs- Threads */
+  uint8_t cvthrds;              /** OpenCV Bildbearbeitungs- Thread */
 };
 
 /** Dieses Feature ist zur Zeit nicht im "binary" enthalten. Um das Feature zu integrieren muss das Projekt mit `cmake` bzw. `ccmake` neu kofniguriert/kompiliert/gelinkt werden. */
 #define UNIMPLEMENTED(feature) { fprintf(stderr, "%s: feature not implemented (%s)\n", argv[0], feature); exit(1); }
 
+/** Standartwerte für Konfigurationsvariablen der oben beschriebenen Datenstruktur */
+static struct cmd_opts opts = { 0,100,0,0,0,640,480,50,90,-1,4,2 };
 
-static struct cmd_opts opts = { 0,100,0,0,0,640,480,50,90,-1 };
 
-
+/**
+ * @name Kommandozeilenhilfe
+ * @brief Eine kurze Übersicht über alle verfügbaren Konfigurationsvariablen
+ */
 static void usage(char* arg0) {
   fprintf(stderr, "\n%s: usage\n"
                   "\t-S             start as daemon (fails if already started)\n"
@@ -65,6 +72,8 @@ static void usage(char* arg0) {
                   "\t-s [sat]       set camera saturation [0..100] default: %u\n"
                   "\t-g [gain]      set camera gain [0..100] default: %u\n"
                   "\t-e [exp]       set exposure [-1..100] default: %d\n"
+                  "\t-t [num]       set robocup thread count default: %u\n"
+                  "\t-c [num]       set opencv thread count default: %u\n"
 #ifdef ENABLE_VIDEO
                   "VIDEO options:\n"
                   "\t-v [file]      save RIFF-avi stream to [file}\n"
@@ -79,12 +88,32 @@ static void usage(char* arg0) {
 #if defined(USE_XWINDOW) || defined(ENABLE_VIDEO)
                   "VIDEO/XWINDOW options:\n"
                   "\t-w [width]     output image width in pixels\n"
-                  "\t-h [heiight]   output image height in pixels\n"
+                  "\t-h [height]   output image height in pixels\n"
 #endif
                   "\t-p             this\n"
-                "\n", arg0, opts.count, opts.sat, opts.gain, opts.exp);
+#ifdef DEBUG
+                  "\t-d             print OpenCV build/hardware information\n"
+#endif
+                "\n", arg0, opts.count, opts.sat, opts.gain, opts.exp, opts.thrds, opts.cvthrds);
 }
 
+/**
+ * @name Debuginformationen ausgeben
+ */
+#ifdef DEBUG
+static void print_opencv_info(void) {
+  std::cout <<"OpenCV DEBUG"<<std::endl
+            <<"[Build Information]"<<std::endl<<cv::getBuildInformation()<<std::endl
+            <<"[HW Support]"<<std::endl
+            <<"CPUs........: "<<cv::getNumberOfCPUs()<<std::endl
+            <<"MMX.........: "<<cv::checkHardwareSupport(CV_CPU_MMX)<<std::endl
+            <<"SSE.........: "
+              <<(cv::checkHardwareSupport(CV_CPU_SSE) == true ? "SSE " : "")
+              <<(cv::checkHardwareSupport(CV_CPU_SSE2) == true ? "SSE2 " : "")
+              <<(cv::checkHardwareSupport(CV_CPU_SSE3) == true ? "SSE3 " : "")
+              <<(cv::checkHardwareSupport(CV_CPU_SSSE3) == true ? "SSSE3 " : "")<<std::endl;
+}
+#endif
 
 /**
  * @name  `main` Funktion
@@ -95,7 +124,7 @@ int main (int argc,char **argv) {
   time_t timer_begin,timer_end;
 
   char c;
-  while ((c = getopt(argc, argv, "SKn:s:g:e:v:xfw:h:p")) != -1) {
+  while ((c = getopt(argc, argv, "SKn:s:g:e:t:c:v:xfw:h:pd")) != -1) {
     if (c == 0xFF) break;
     switch (c) {
 
@@ -120,6 +149,14 @@ int main (int argc,char **argv) {
       /*######################*/
       case 'e':
         opts.exp       = strtoul(optarg, NULL, 10);
+        break;
+      /*######################*/
+      case 't':
+        opts.thrds     = strtoul(optarg, NULL, 10);
+        break;
+      /*######################*/
+      case 'c':
+        opts.cvthrds   = strtoul(optarg, NULL, 10);
         break;
       /*######################*/
       case 'v':
@@ -162,6 +199,15 @@ int main (int argc,char **argv) {
 #endif
         break;
       /*####################*/
+      case 'd':
+#ifdef DEBUG
+        print_opencv_info();
+#else
+        UNIMPLEMENTED("DEBUG");
+#endif
+        exit(1);
+        break;
+      /*####################*/
       case 'p':
       default:
         usage(argv[0]);
@@ -169,40 +215,51 @@ int main (int argc,char **argv) {
     }
   }
 
+  /* Anwendung als Diesnt im Hintergrund starten (falls aktiviert) */
   if (opts.daemonize == true) {
     if (rc::Daemon::instance()->Daemonize(PIDFILE, LOCKFILE, CHUSER) == false)
       return 1;
   } else {
     rc::setSignals();
   }
-  
-  rc::BlobDetectorFactory detector(4);
+
+  /* Initialisierung (Kamera, OpenCV, etc) */
+  rc::BlobDetectorFactory detector(opts.thrds);
   detector.setSaturation(opts.sat);
   detector.setGain(opts.gain);
   detector.setExposure(opts.exp);
 #ifdef USE_XWINDOW
+  /* Vorschaufenster Optionen setzen */
   detector.getXWindow()->setXWindow( opts.useXWindow, opts.showFiltered );
 #endif
 #ifdef ENABLE_VIDEO
+  /* Ausgabedatei des Videos festlegen */
   detector.setVideoOut(opts.videoFile);
 #endif
+#if defined(USE_XWINDOW) || defined(ENABLE_VIDEO)
+  /* Fenstergröße (Vorschau) festlegen */
+  detector.setDimensions(opts.width, opts.height);
+#endif
 
+  /* Kamera initialisieren */
   if (!detector.openCamera()) {
     std::cerr << "Error opening the camera"<<std::endl;
     return -1;
   }
-#if defined(USE_XWINDOW) || defined(ENABLE_VIDEO)
-  detector.setDimensions(opts.width, opts.height);
-#endif
-  std::cout <<"Resolution: "<<detector.getWidth()<<"x"<<detector.getHeight()<<std::endl
-            <<"Format....: "<<detector.getFormat()<<std::endl
-            <<"Saturation: "<<detector.getSaturation()<<std::endl
-            <<"Gain......: "<<detector.getGain()<<std::endl
-            <<"Exposure..: "<<detector.getExposure()<<std::endl
-            <<"------------------------------"<<std::endl;
-  std::cout <<"OpenCV settings"<<std::endl
-            <<"Threads...: "<<cv::getThreadNum()<<std::endl;
+  /* Thread Anzahl (OpenCV Threads -> zur Zeit nicht verfügbar) */
+  cv::setNumThreads(opts.cvthrds);
+  /* optimierten OpenCV code ausführen (bei Raspberrypi nicht notwendig da kein mmx oder sse vorhanden */
+  cv::setUseOptimized(true);
 
+  /* ein paar Konfigurationsvariablen ausgeben */
+  std::cout <<"Resolution..: "<<detector.getWidth()<<"x"<<detector.getHeight()<<std::endl
+            <<"Format......: "<<detector.getFormat()<<std::endl
+            <<"Saturation..: "<<detector.getSaturation()<<std::endl
+            <<"Gain........: "<<detector.getGain()<<std::endl
+            <<"Exposure....: "<<detector.getExposure()<<std::endl
+            <<"OpenCV-Thrds: "<<cv::getNumThreads()<<std::endl
+            <<"Optimised...: "<<cv::useOptimized()<<std::endl
+            <<"Threads.....: "<<static_cast<int>(opts.thrds)<<std::endl;
 #ifdef ENABLE_VIDEO
   if (opts.videoFile)
     std::cout <<"Saving RIFF-avi stream to file: "<<opts.videoFile<<std::endl;
@@ -213,21 +270,29 @@ int main (int argc,char **argv) {
   if (opts.showFiltered)
     std::cout <<"Open X11 preview window (filtered image)"<<std::endl;
 #endif
+
+  /* Startzeit feststellen (für FPS Berechnung) */
   time(&timer_begin);
+  /* Arbeiter- Threads starten */
   detector.startThreads();
+  /* Spaltenberzeichnung ausgeben */
   std::cout <<"------------------------------"<<std::endl
             <<"[Elapsed] [Images] [FPS] [BufferInfo]"<<std::endl;
 
   double secondsElapsed = 0.0f;
   unsigned int i = 0, n = 1;
+  /* Hauptschleife */
   while (i <= opts.count && !rc::isDaemonTerminate()) {
+    /* Kamerabild holen */
     while (!detector.grabCameraImage()) {
       std::this_thread::yield();
       continue;
     }
     if (!opts.daemonize && (n%3 == 1 || n == opts.count)) {
+      /* vergangen Zeit merken (für FPS Berechnung) */
       time(&timer_end);
       secondsElapsed = difftime(timer_end,timer_begin);
+      /* Informationen ausgeben (u.a. FPS) */
       std::cout <<"\r["<<std::fixed<<std::setprecision(0)<<std::setw(7)<<secondsElapsed<<"] "
                   <<"["<<std::setw(6)<<n<<"] "
                   <<"["<<std::setw(3)<<std::setprecision(0)<<(float)(secondsElapsed > 0 ? (float)(n/secondsElapsed) : 0.0)<<"] "
@@ -238,9 +303,11 @@ int main (int argc,char **argv) {
     n++;
   }
   std::cout <<std::endl;
+  /* Arbeiter- Threads stoppen */
   detector.stopThreads();
+  /* Kamera deinitialisieren */
   detector.closeCamera();
-
+  /* durchschnittliche FPS ausgeben */
   std::cout <<"FPS: "<<std::setprecision(10)<<(float)(n/secondsElapsed)<<std::endl;
   return 0;
 }
