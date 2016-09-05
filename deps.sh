e6ef9540f45167c55792b4adbd2f9afebc9081f4
Matze ton <matzeton@googlemail.com>
2016-09-05 14:42:00 +0000

    deps.sh edited online with Bitbucket

--- deps.sh
+++ deps.sh
@@ -6,9 +6,13 @@ if [ $(id -u) -ne 0 ]; then
 	exit 1
 fi
 
+# Raspberrypi Pakete installieren (Bitte nur diese nutzen!)
 dpkg -i $(dirname $0)/dependencies/*.deb
+# Build Enviroment
 apt-get install gcc autoconf cmake make cmake-curses-gui
 apt-get install dh-make build-essential
 apt-get install devscripts fakeroot debootstrap pbuilder
+# OpenCV Bibliotheken + Header
 apt-get install libopencv-dev libmicrohttpd-dev
+# doxygen + UML Generator
 apt-get install doxygen graphviz
